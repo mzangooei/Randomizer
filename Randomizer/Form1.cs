68031594c15b10270baa03d4378030df05587b08
Manoochehr Zangooei <manoochehr.zangooei@gmail.com>
2018-03-13 08:51:16 +0330

    insignificant code cleaning
    insignificant code cleaning

--- Randomizer/Form1.cs
+++ Randomizer/Form1.cs
@@ -26,21 +26,16 @@ namespace Randomizer
             Random rnd1 = new Random();
 
             richTextBox1.Clear();
-         
-           // int j = 0;
+
             for (int i = 0; i < set1; i++)
             {
-              //  richTextBox1.Text +=  + bytes1[ctr] + ",";
-              //if ((ctr + 1) % 10 == 0)  richTextBox1.Text +="\n";
+
                 int1[i] = rnd1.Next(Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text)+1);
-               // richTextBox1.Text += i.ToString();
-              //  richTextBox1.Text += int1[i].ToString()+" | ";
-             //   if ((i + 1) % 10 == 0)  richTextBox1.Text +="\n";
+
                 if (!randomList.Contains(int1[i]))
                 {
                     randomList.Add(int1[i]);
-                    //   richTextBox1.Text += randomList[j].ToString()+" | ";
-                    // j++;
+
                 }
                 else i--;
             }
@@ -50,7 +45,6 @@ namespace Randomizer
            foreach (int x in randomList)
            {
                richTextBox1.Text += randomList[j].ToString() + ", ";
-            //   if ((j + 1) % 10 == 0) richTextBox1.Text += "\n";
                j++;
            }
 
@@ -67,27 +61,14 @@ namespace Randomizer
 
 
         private PrintDocument docToPrint = new PrintDocument();    
-      
-
-
 
         private void PrintPage(object sender, PrintPageEventArgs e)
         {
-//string text = "This text to be printed. ";
-       //     e.Graphics.DrawString("\n " + "1 set of " + textBox1.Text + " Unique Numbers \n Range: From " + textBox2.Text + " to " + textBox3.Text + " \n\n Results:\n  " + richTextBox1.Text, new Font("Segoe UI", 15),
-           //     Brushes.Black, 35, 20);
-            //Graphics gf = e.Graphics;
-            //SizeF sf = gf.MeasureString("\n " + "1 set of " + textBox1.Text + " Unique Numbers \n Range: From " + textBox2.Text + " to " + textBox3.Text + " \n\n Results:\n  " + richTextBox1.Text,
-            //                 new Font(new FontFamily("Arial"), 10F), 700);
 
             RectangleF drawRect = new RectangleF(45, 40, 740,990);
   e.Graphics.DrawString("\n " + "1 Set of " + textBox1.Text + " Unique Numbers \n Range: From " + textBox2.Text + " to " + textBox3.Text + " \n\n Results:\n " + richTextBox1.Text, new Font("Segoe UI", 13),
               Brushes.Black, drawRect,
                          StringFormat.GenericTypographic);
-        
-
-
-            
         }
 
 
@@ -107,10 +88,8 @@ namespace Randomizer
             printPreviewDialog1.Dock = DockStyle.Fill;
             printPreviewDialog1.Location = new Point(88, 80);
             printPreviewDialog1.Document = docToPrint;
-            //     printPreviewDialog1.Zoom = 0.25;
             printPreviewDialog1.Document.DocumentName = "Research Randomizer";
             printPreviewDialog1.UseAntiAlias = true;
-            // Add PrintDocument PrintPage event handler
             this.docToPrint.PrintPage +=
             new System.Drawing.Printing.PrintPageEventHandler(PrintPage);
             printPreviewDialog1.ShowDialog();
@@ -128,52 +107,25 @@ namespace Randomizer
 
         private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
         {
-        //    if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
-        //(e.KeyChar != '.'))
-        //    {
-               
-        //        e.Handled = true;
 
-        //    }
-
-            //int isNumber = 0;
-            //e.Handled = !int.TryParse(e.KeyChar.ToString(), out isNumber);
             e.Handled = !(Char.IsNumber(e.KeyChar) || e.KeyChar == 8);
         }
 
         private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
         {
-        //    if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
-        //(e.KeyChar != '.'))
-        //    {
-
-               
-        //        e.Handled = true;
-        //    }
-            //int isNumber = 0;
-            //e.Handled = !int.TryParse(e.KeyChar.ToString(), out isNumber);
 
             e.Handled = !(Char.IsNumber(e.KeyChar) || e.KeyChar == 8);
         }
 
         private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
         {
-        //    if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
-        //(e.KeyChar != '.'))
-        //    {
 
-        //        e.Handled = true;
-        //    }
-            //int isNumber = 0;
-            //e.Handled = !int.TryParse(e.KeyChar.ToString(), out isNumber);
             e.Handled = !(Char.IsNumber(e.KeyChar) || e.KeyChar == 8);
          
         }
 
         private void Form1_KeyPress(object sender, KeyPressEventArgs e)
         {
-
-         
 
         }
 
@@ -193,21 +145,14 @@ namespace Randomizer
             {
 
                 if (Convert.ToInt32(textBox2.Text) < Convert.ToInt32(textBox3.Text))
-                    // if (textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "")
                     button1.Enabled = true;
                 else button1.Enabled = false;
-
-           //     label3.Text=Convert.ToString((Convert.ToInt32(textBox1.Text))<=(Convert.ToInt32(textBox3.Text) - Convert.ToInt32(textBox2.Text)));
 
                 if ((Convert.ToInt32(textBox1.Text)) >= (Convert.ToInt32(textBox3.Text) - Convert.ToInt32(textBox2.Text))+2)
                 {
                     button1.Enabled = false;
                     button2.Enabled = false;
                 }
-
-                   
-
-            
 
             }
 
@@ -216,15 +161,6 @@ namespace Randomizer
                 button1.Enabled = false;
                 button2.Enabled = false;
             }
-
-            //if (textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "")
-            //if ((Convert.ToInt32(textBox1.Text))<=(Convert.ToInt32(textBox3.Text) - Convert.ToInt32(textBox2.Text))) 
-            //{
-
-            //    button1.Enabled = false;
-            //    button2.Enabled = false;
-            //}
-
 
             if (richTextBox1.Text == "")
                 button2.Enabled = false;
